9a78363916d6f5e7a5c9966c6676901591ae7765
AmritRajeev <44172582+AmritRajeev@users.noreply.github.com>
2019-10-01 18:34:34 +0530

    Update kalpana.py

--- kalpana.py
+++ kalpana.py
@@ -5,6 +5,12 @@ from alpha_vantage.timeseries import TimeSeries
 import time
 from sklearn.ensemble import RandomForestClassifier
 api_key="MNNSPRF08WG4DTL3"
-time_key = TimeSeries(key=api_key,output_format="pandas")
+time_series = TimeSeries(key=api_key,output_format="pandas")
 data,meta_data=time_series.get_daily(symbol="MSFT",outputsize="full")
-
+data=data.rename(columns={
+            '1.open' : 'Open',
+            '2.high' : 'High',
+            '3.low' : 'Low',
+            '4.close' : 'Close',
+            '5.volume' : 'Volume',
+                         })
