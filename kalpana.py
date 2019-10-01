3f5782904f877d772b43034b8af521f4d9d46cad
AmritRajeev <44172582+AmritRajeev@users.noreply.github.com>
2019-10-01 19:13:47 +0530

    Update kalpana.py

--- kalpana.py
+++ kalpana.py
@@ -8,18 +8,20 @@ api_key="MNNSPRF08WG4DTL3"
 time_series = TimeSeries(key=api_key,output_format="pandas")
 data,meta_data=time_series.get_daily(symbol="MSFT",outputsize="full")
 data=data.rename(columns={
-            '1.open' : 'Open',
-            '2.high' : 'High',
-            '3.low' : 'Low',
-            '4.close' : 'Close',
-            '5.volume' : 'Volume',
+            '1. open' : 'Open',
+            '2. high' : 'High',
+            '3. low' : 'Low',
+            '4. close' : 'Close',
+            '5. volume' : 'Volume',
                          })
-data['Open-Close'] = (data.Open - data.Close)/data.Open
+data['open-close'] = (data.Open - data.Close)/data.Open
 data['High-Low'] = (data.High - data.Low)/data.Low
-data['percent_change'] = data['Close'].pct_change()
+data['percent_change'] = data['close'].pct_change()
 data['std_5']=data['percent_change'].rolling(5).std()
 data['ret_5']=data['percent_change'].rolling(5).mean()
 data.dropna(inplace=TRUE)
+
+
 
 
 
